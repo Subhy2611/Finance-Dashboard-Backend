b6431f35eca297eca68f7d2ac844df22702fea65
Subhy2611 <isubhamghosh26@gmail.com>
2026-04-05 11:57:35 +0530

    Route configuration of recent records

--- src/routes/record.routes.js
+++ src/routes/record.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+
 const { createRecord, getRecords, getDashboard, getRecentRecords } = require('../controllers/record.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+
 
 // Create record
 router.post('/', authMiddleware, createRecord);
