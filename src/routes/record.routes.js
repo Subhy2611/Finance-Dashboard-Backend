29f38354934bd1b0807f054ea6108d98c8201928
Subhy2611 <isubhamghosh26@gmail.com>
2026-04-05 11:53:02 +0530

    Get recent records

--- src/routes/record.routes.js
+++ src/routes/record.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { createRecord, getRecords, getDashboard } = require('../controllers/record.controller');
+const { createRecord, getRecords, getDashboard, getRecentRecords } = require('../controllers/record.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 // Create record
@@ -9,6 +9,9 @@ router.post('/', authMiddleware, createRecord);
 
 // Get records
 router.get('/', authMiddleware, getRecords);
+
+// Get recent records
+router.get('/recent', authMiddleware, getRecentRecords);
 
 // Dashboard data
 router.get('/dashboard', authMiddleware, getDashboard);
